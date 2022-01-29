9d9b615e3af2efc4f277aad0b42230ded905beac
Maatari Okouya <maatariokouya@gmail.com>
2022-01-29 17:56:56 +0000

    clean code

--- strings/src/Print3.hs
+++ strings/src/Print3.hs
@@ -1,16 +1,24 @@
 module Print3 where
 
+  
+  
+{- 
+  Note:
+  -----
+  String is a type alias for [char] 
+-}
 
+myGreetings :: String
 myGreetings = "hello " ++ " world!"
 
+hello :: String
 hello = "Hello"
 
+world :: String
 world = "world"
 
 main :: IO ()
 main = do
-        putStrLn helloWorld
-        where
-          helloWorld = concat [hello, " " , world]
-
-
+  putStrLn helloWorld
+  where
+    helloWorld = concat [hello, " ", world]
