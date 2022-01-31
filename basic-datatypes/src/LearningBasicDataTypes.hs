d8e47301c9674d56532efdcacffa01f55f8053a1
Maatari Okouya <maatariokouya@gmail.com>
2022-01-31 16:05:43 +0000

    add quick comment

--- basic-datatypes/src/LearningBasicDataTypes.hs
+++ basic-datatypes/src/LearningBasicDataTypes.hs
@@ -1,9 +1,10 @@
 module LearningBasicDataTypes where
   
-  
+
+-- Basic Data Types definition  
 data Mood = Blah | Woot deriving Show
 
-
+-- Pattern Matching example
 changeMood :: Mood -> Mood
 changeMood Blah = Woot
 changeMood  _   = Blah
